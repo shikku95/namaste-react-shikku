723bd454fd44741dac310b9650ec3894ea2309cd
Shikku <shikku.intimazione@gmail.com>
2025-02-20 00:54:16 +0530

    Episode 2 completed

--- app.js
+++ app.js
@@ -1,3 +1,6 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+
 const heading = React.createElement(
   "h1",
   { id: "heading" },
@@ -5,5 +8,3 @@ const heading = React.createElement(
 );
 const root = ReactDOM.createRoot(document.getElementById("root"));
 root.render(heading);
-const roots = ReactDOM.createRoot(document.getElementById("roots"));
-roots.render(heading);
