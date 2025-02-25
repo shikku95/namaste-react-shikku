54b695b4a87f39e3e8bc01671a15fee1e517235d
Shikku <shikku.intimazione@gmail.com>
2025-02-25 23:39:51 +0530

    ep 3 completed

--- app.js
+++ app.js
@@ -1,10 +1,9 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
-const heading = React.createElement(
-  "h1",
-  { id: "heading" },
-  "Hello world from REACT"
-);
+const FunctionalComponent = () => {
+  return <h1>Welcome to functional Components</h1>;
+};
+
 const root = ReactDOM.createRoot(document.getElementById("root"));
-root.render(heading);
+root.render(FunctionalComponent());
